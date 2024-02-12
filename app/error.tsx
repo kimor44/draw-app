6f8811a4891788e46e24245afe449fff4c256246
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2024-02-12 14:53:32 +0100

    Ajouter le digest d'erreur à la page d'erreur

--- app/error.tsx
+++ app/error.tsx
@@ -13,6 +13,7 @@ const ErrorBoundary = ({
     <div>
       <h2>Something went wrong</h2>
       <p>{error.message}</p>
+      <p>{error.digest}</p>
       <p>
         <Link className="cursor-pointer underline" href="/">
           Back to Home
