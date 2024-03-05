d6ff53bfbc57d69363d8eff4732d4f4b51683ebc
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2024-03-05 10:47:43 +0100

    Add Zod & apply to addCandidateAction close #21

--- app/src/actions/candidate/addCandidateAction.ts
+++ app/src/actions/candidate/addCandidateAction.ts
@@ -1,6 +1,24 @@
 "use server";
 import { prisma } from "@/app/lib/prisma/_base";
 import { cookies } from "next/headers";
+import { z } from "zod";
+
+const CandidateSchema = z.object({
+  name: z.string(),
+});
+
+type TAddCandidateAction = z.infer<typeof CandidateSchema>;
+
+const newCandidateSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  isRemaining: z.boolean(),
+  sessionID: z.string().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+type TNewCandidate = z.infer<typeof newCandidateSchema>;
 
 export const addCandidateAction = async (formData: FormData) => {
   let sessionID = cookies().get("session-id");
@@ -12,16 +30,18 @@ export const addCandidateAction = async (formData: FormData) => {
     sessionID = cookies().get("session-id");
   }
 
-  const name = formData.get("name");
+  const name = String(formData.get("name"));
 
-  if (!name) {
+  const newName: TAddCandidateAction = CandidateSchema.parse({ name: name });
+
+  if (!newName.name) {
     return { error: "Name is required" };
   }
 
   try {
-    const newCandidate = await prisma.candidate.create({
+    const newCandidate: TNewCandidate = await prisma.candidate.create({
       data: {
-        name: name as string,
+        name: newName.name,
         sessionID: sessionID?.value,
       },
     });
