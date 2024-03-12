360fe97b2395b05fc376f715c6440570f25036c9
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2024-03-12 16:02:17 +0100

    Adapt draw modal content when no user remaining close #9

--- app/features/home/DrawModal.tsx
+++ app/features/home/DrawModal.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { TCandidate } from '@/app/features/home/Candidates';
-import { RemainingCandidates } from '@/app/features/home/RemainingCandidates';
-import { LaunchNewDraw } from '@/app/features/home/LaunchNewDraw';
+import { RemainingCandidates } from './RemainingCandidates';
+import { LaunchNewDraw } from './LaunchNewDraw';
 
 type TDrawModal = {
   isOpen: boolean;
@@ -17,13 +17,16 @@ const DrawModal: React.FC<TDrawModal> = ({
   candidates,
   onActionChange
 }: TDrawModal) => {
+  const hasCandidates = candidates.length > 0;
   return (
     <div
       className={`${
         isOpen ? 'block' : 'hidden'
       } fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center`}
     >
-      <div className="bg-background dark:bg-text w-1/2 m-auto p-4 rounded-lg flex flex-col gap-5">
+      <div
+        className={`bg-background dark:bg-text w-1/2 m-auto p-4 rounded-lg ${hasCandidates ? '' : 'pb-10'}`}
+      >
         <div className="flex justify-end">
           <button
             className="text-text dark:text-background font-bold rounded px-4 py-2"
@@ -32,14 +35,20 @@ const DrawModal: React.FC<TDrawModal> = ({
             X
           </button>
         </div>
-        <h1 className="text-2xl font-bold text-center text-text dark:text-background">
-          Candidates remaining
-        </h1>
-        <RemainingCandidates candidates={candidates} />
-        <LaunchNewDraw
-          candidates={candidates}
-          onActionChange={onActionChange}
-        />
+        <section className="flex flex-col gap-5">
+          <h1 className="text-2xl font-bold text-center text-text dark:text-background">
+            {hasCandidates ? 'Candidates remaining' : 'No candidates remaining'}
+          </h1>
+          {hasCandidates && (
+            <>
+              <RemainingCandidates candidates={candidates} />
+              <LaunchNewDraw
+                candidates={candidates}
+                onActionChange={onActionChange}
+              />
+            </>
+          )}
+        </section>
       </div>
     </div>
   );
